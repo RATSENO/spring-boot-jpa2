017b8e1aed6c1936ad6850748827c876e601bf37
ratseno <onestar1014@gmail.com>
2019-03-10 16:02:53 +0900

    [ADD]스프링 데이터 Common: 커스텀 리포지토리 프로젝트

---
+++ src/main/java/me/ratseno/demojpa3/Demojpa3Application.java
@@ -0,0 +1,13 @@
+package me.ratseno.demojpa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojpa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demojpa3Application.class, args);
+	}
+
+}
